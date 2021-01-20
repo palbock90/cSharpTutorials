7e7a14c1b69f001c8e9a4b5ca31bf1fdbcda93cb
Pål Bøckmann <plbockmann@gmail.com>
2021-01-20 12:41:38 +0100

    first commit

---
+++ Delegates/Delegates/DemoLibrary/ShoppingCartModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DemoLibrary
+{
+    public class ShoppingCartModel
+    {
+        public delegate void MentionDiscout(decimal subTotal);
+
+        public List<ProductModel> Items { get; set; } = new List<ProductModel>();
+
+        public decimal GenerateTotal(MentionDiscout mentionSubtotal, 
+            Func<List<ProductModel>, decimal, decimal> calculateDiscountedTotal,
+            Action<string> tellUserWeAreDiscounting)
+        {
+            decimal subTotal = Items.Sum(x => x.Price);
+
+            mentionSubtotal(subTotal);
+
+            tellUserWeAreDiscounting("We are applying your discount.");
+
+            return calculateDiscountedTotal(Items, subTotal);
+        }
+    }
+
+}
+
---
+++ Logging/Logging/Logging/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Logging.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogInformation("This is our first logged message");
+        }
+    }
+}
